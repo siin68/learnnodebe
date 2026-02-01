8f3e27b5c68dd4e01c9626bcc9f1f22e3e24d421
siin68 <tranhoangthin68@gmail.com>
2026-02-01 22:08:17 +0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello, World!');
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+app.get('/', (req, res) => {
+  res.send('ahahahahah');
+});
+app.post('/haha', (req, res) => {
+  res.send('Post request to the homepage');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
